5564b4d5659bc78c87cef1099d99d734efc53e5b
Mohammed <mohammed.secure@outlook.sa>
2026-06-21 21:19:59 +0300

    Task 1: Fix print_square.js (change radix from 16 to 10)

---
+++ challenge/1-print_square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.exit(1);
+}
+
+const size = parseInt(process.argv[2], 10);
+
+for (let i = 0 ; i < size ; i ++) {
+    let string = "";
+    for (let j = 0 ; j < size ; j ++) {
+        string += "#";
+    }
+    console.log(string);
+}
